b590458654ad74a5c047b2605499adcf7392448a
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-29 17:26:54 -0300

    server.js terminado

--- src/server.js
+++ src/server.js
@@ -1,16 +1,31 @@
+// Servidor
 const express = require('express')
 const server = express()
- 
-server.use(express.static("public"))
 
-.get("/", (req, res) => {
-  return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/", (req, res) => {
-  return res.sendFile(__dirname + "/views/study.html")
-})
-.get("/", (req, res) => {
-  return res.sendFile(__dirname + "/views/give-classes.html")
+const  {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
+
+//configurar nunjucks (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
 
+// Inicio e configuração do servidor
+server
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+// configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+// rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+// start do servidor
 .listen(5500)
