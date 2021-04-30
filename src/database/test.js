f1255a667fb260056e07406203f810a3441f6480
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-30 08:43:16 -0300

    selectClassesAndProffys feito

--- src/database/test.js
+++ src/database/test.js
@@ -5,7 +5,7 @@ const createProffy = require('./createProffy')
 Database.then(async (db) => {
   // Inserir dados
   proffyValue = {
-    name: "Diego Fernandes", 
+    name: "Diogo Sales", 
     avatar: "https://avatars.githubusercontent.com/u/72995630?v=4", 
     whatsapp: "89987654534", 
     bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
@@ -30,4 +30,21 @@ Database.then(async (db) => {
         time_to: 1220
     }
   ]
+
+  // await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+  // Consultar os dados inseridos
+
+  // todos os proffys
+  const selectedProffys = await db.all("SELECT * FROM proffys")
+  // console.log(selectedProffys)
+
+  // consultar as classes de um determinado professor
+  // e trazer junto os dados do professor
+  const selectClassesAndProffys = await db.all(`
+    SELECT classes.*, proffys.*
+    FROM proffys
+    JOIN classes ON (classes.proffy_id = proffys.id)
+    WHERE classes.proffy_id = 1;
+  `)
 })
