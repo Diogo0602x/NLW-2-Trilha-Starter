7f006d48cbe3d11ccf3b46332a81b290c3b3ec8a
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-30 08:54:50 -0300

    const insertedClassSchedule feito

--- src/database/createProffy.js
+++ src/database/createProffy.js
@@ -30,4 +30,24 @@ module.exports = async function(db, { proffyValue, classValue, classScheduleValu
   `)
 
   const class_id = insertedClass.lastID
+  
+  // Inserir dados na tabela class_schedule
+  const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+    return db.run(`
+      INSERT INTO class_schedule (
+        class_id,
+        weekday,
+        time_from,
+        time_to
+      ) VALUES (
+        "${class_id}",
+        "${classScheduleValue.weekday}",
+        "${classScheduleValue.time_from}",
+        "${classScheduleValue.time_to}"
+      );
+    `)
+  })
+
+  // aqui vou executar todos os db.runs() das class_schudules
+  await Promise.all(insertedAllClassScheduleValues)
 }
