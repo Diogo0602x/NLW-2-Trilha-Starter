9faff0259c5775a75c131c274585af1e2fc9178c
Diogo0602x <diogoteixeira0602@gmail.com>
2021-04-30 08:31:33 -0300

    extensão sqlite instalada

--- src/database/db.js
+++ src/database/db.js
@@ -4,26 +4,26 @@ function execute(db) {
   // criar as tabelas do banco de dados.
   return db.exec(`
     CREATE TABLE IF NOT EXISTS proffys (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        avatar TEXT,
-        whatsapp TEXT,
-        bio TEXT
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      avatar TEXT,
+      whatsapp TEXT,
+      bio TEXT
     );
 
     CREATE TABLE IF NOT EXISTS classes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        subject INTEGER,
-        cost TEXT,
-        proffy_id INTEGER
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      subject INTEGER,
+      cost TEXT,
+      proffy_id INTEGER
     );
 
     CREATE TABLE IF NOT EXISTS class_schedule (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        class_id INTEGER,
-        weekday INTEGER,
-        time_from INTEGER,
-        time_to INTEGER
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      class_id INTEGER,
+      weekday INTEGER,
+      time_from INTEGER,
+      time_to INTEGER
     );
   `)
 }
